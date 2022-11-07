2e18b718af188114845305cb80ccf52890546924
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-11-07 11:07:54 -0300

    kek

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
-import axios from "axios";
+import Unsplash from "./api/Unsplash";
+import { ImageList } from "./components/ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await Unsplash.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term,
       },
-      headers: {
-        Authorization:
-          "Client-ID ICGtgey656l7QdcJhTxWwwOCsEtTAnOCOzCbFeg79Xs",
-      },
     });
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
---
+++ src/components/ImageList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ImageList = (props) => {
+
+  console.log(props.images);
+  return (
+    <div>ImageList</div>
+  )
+}
