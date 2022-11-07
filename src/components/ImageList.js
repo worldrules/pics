422ad48f4fb88c0284c139e0b2a89ae3102d6722
Leonardo Nunes Carvalho <worldrulesdev@gmail.com>
2022-11-07 15:02:35 -0300

    kek

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export const ImageList = (props) => {
   // console.log(this.props)
-  const images = props.images.map((image)=>{
-    return <img src={image.urls.regular}/>
-  })
+  const images = props.images.map(({ description, id, urls}) => {
+    return (
+      <img alt={description} key={id} src={urls.regular} />
+    );
+  });
 
-  return (
-    <div>{images}</div>
-  )
-}
+  return <div>{images}</div>;
+};
